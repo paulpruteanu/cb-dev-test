267d1e6b427b2dbe11fb9667db96748cecdbc304
Paul Dan Pruteanu <paul.pruteanu@contingent.ai>
2022-07-17 15:37:56 +0300

    re-factory

--- src/services/paymentService.interface.ts
+++ src/services/paymentService.interface.ts
@@ -1,5 +1,11 @@
-import { MakePaymentRequest, MakePaymentResult } from "../types";
+import { Account, MakePaymentRequest, MakePaymentResult, PaymentScheme } from "../types";
+import AccountDatastore from "../data/accountDatastore";
+import BackupAccountDatastore from "../data/backupAccountDatastore";
 
 export default interface PaymentServiceInterface {
+  datastoreType: string;
+  accountDatastore: AccountDatastore | BackupAccountDatastore;
   makePayment(request: MakePaymentRequest): MakePaymentResult;
+  getAccountDatastore(): AccountDatastore | BackupAccountDatastore;
+  isValidPayment(paymentScheme: PaymentScheme, account: Account, request: MakePaymentRequest): boolean;
 }
--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -1,75 +1,73 @@
 import {
   Account,
+  AccountStatus,
+  AllowedPaymentSchemes,
   MakePaymentRequest,
   MakePaymentResult,
   PaymentScheme,
-  AccountStatus,
-  AllowedPaymentSchemes,
-} from '../types';
-import DatastoreConfig from '../config/datastore';
-import BackupAccountDatastore from '../data/backupAccountDatastore';
-import AccountDatastore from '../data/accountDatastore';
-import PaymentServiceInterface from './paymentService.interface';
+} from "../types";
+import DatastoreConfig from "../config/datastore";
+import BackupAccountDatastore from "../data/backupAccountDatastore";
+import AccountDatastore from "../data/accountDatastore";
+import PaymentServiceInterface from "./paymentService.interface";
 
 export default class PaymentService implements PaymentServiceInterface {
   datastoreType: string;
+  accountDatastore: AccountDatastore | BackupAccountDatastore;
   constructor() {
     const datastoreConfig = new DatastoreConfig();
     this.datastoreType = datastoreConfig.getConfig().datastoreType;
+    this.accountDatastore = this.getAccountDatastore();
   }
+  getAccountDatastore(): AccountDatastore | BackupAccountDatastore {
+    return this.datastoreType === "Backup"
+      ? new BackupAccountDatastore()
+      : new AccountDatastore();
+  }
+  paymentSchemeMapping = {
+    [AllowedPaymentSchemes.FasterPayments]: PaymentScheme.FasterPayments,
+    [AllowedPaymentSchemes.Bacs]: PaymentScheme.Bacs,
+    [AllowedPaymentSchemes.Chaps]: PaymentScheme.Chaps,
+  };
+  isValidPayment(
+    paymentScheme: PaymentScheme,
+    account: Account,
+    request: MakePaymentRequest
+  ): boolean {
+    if (
+      account === null ||
+      this.paymentSchemeMapping[account.allowedPaymentSchemes] !== paymentScheme
+    ) {
+      return false;
+    }
+    if (
+      paymentScheme === PaymentScheme.FasterPayments &&
+      account.balance < request.amount
+    ) {
+      return false;
+    }
+    return !(
+      paymentScheme === PaymentScheme.Chaps &&
+      account.accountStatus !== AccountStatus.Live
+    );
+  }
+
   makePayment(request: MakePaymentRequest): MakePaymentResult {
-    let account: Account;
+    let account: Account = this.accountDatastore.getAccount(
+      request.debtorAccountNumber
+    );
 
-    if (this.datastoreType === 'Backup') {
-      let accountDatastore = new BackupAccountDatastore();
-      account = accountDatastore.getAccount(request.debtorAccountNumber);
-    } else {
-      let accountDatastore = new AccountDatastore();
-      account = accountDatastore.getAccount(request.debtorAccountNumber);
+    if (!this.isValidPayment(request.paymentScheme, account, request)) {
+      return {
+        success: false,
+      };
     }
 
-    let result: MakePaymentResult = { success: true };
+    account.balance -= request.amount;
+    this.accountDatastore.updateAccount(account);
 
-    switch (request.paymentScheme) {
-      case PaymentScheme.Bacs:
-        if (account === null) {
-          result.success = false;
-        } else if (account.allowedPaymentSchemes !== AllowedPaymentSchemes.Bacs) {
-          result.success = false;
-        }
-        break;
-      case PaymentScheme.FasterPayments:
-        if (account === null) {
-          result.success = false;
-        } else if (account.allowedPaymentSchemes !== AllowedPaymentSchemes.FasterPayments) {
-          result.success = false;
-        } else if (account.balance < request.amount) {
-          result.success = false;
-        }
-        break;
-      case PaymentScheme.Chaps:
-        if (account === null) {
-          result.success = false;
-        } else if (account.allowedPaymentSchemes !== AllowedPaymentSchemes.Chaps) {
-          result.success = false;
-        } else if (account.accountStatus !== AccountStatus.Live) {
-          result.success = false;
-        }
-        break;
-    }
-
-    if (result.success) {
-      account.balance -= request.amount;
-
-      if (this.datastoreType === 'Backup') {
-        let accountDatastore = new BackupAccountDatastore();
-        accountDatastore.updateAccount(account);
-      } else {
-        let accountDatastore = new AccountDatastore();
-        accountDatastore.updateAccount(account);
-      }
-    }
-
-    return result;
+    return {
+      success: true,
+    };
   }
 }
